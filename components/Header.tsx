5fa0f30f7214e8401fd8c8f1ebba7273b9528e09
Tomy Visouthiphongs <tvisouthiphong@gmail.com>
2023-04-08 15:06:17 +0200

    Add quick & dirty header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,56 +1,121 @@
-import React from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
+// Header.tsx
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { signOut, useSession } from 'next-auth/react';
 
 const Header: React.FC = () => {
   const router = useRouter();
   const isActive: (pathname: string) => boolean = (pathname) =>
     router.pathname === pathname;
 
+  const { data: session, status } = useSession();
+
   let left = (
-    <div className="left">
-      <Link href="/">
-        <a className="bold" data-active={isActive("/")}>
+    <div>
+      <Link href="/" className="bold" data-active={isActive('/')} style={{fontWeight: "bold", ...styles.nextLink, color: isActive('/') ? "black" : "gray"}}>
           Feed
-        </a>
       </Link>
-      <style jsx>{`
-        .bold {
-          font-weight: bold;
-        }
-
-        a {
-          text-decoration: none;
-          color: #000;
-          display: inline-block;
-        }
-
-        .left a[data-active="true"] {
-          color: gray;
-        }
-
-        a + a {
-          margin-left: 1rem;
-        }
-      `}</style>
     </div>
   );
 
   let right = null;
 
+  if (status === 'loading') {
+    left = (
+      <div className="left">
+        <Link href="/" className="bold" data-active={isActive('/')} style={{fontWeight: "bold", ...styles.nextLink, color: isActive('/') ? "black" : "gray"}}>
+            Feed
+        </Link>
+      </div>
+    );
+    right = (
+      <div className="right" style={styles.right}>
+        <p>Validating session ...</p>
+      </div>
+    );
+  }
+
+  if (!session) {
+    right = (
+      <div className="right" style={styles.right}>
+        <Link href="/api/auth/signin" style={{fontWeight: 'bold', ...styles.nextLink, color: isActive('/') ? 'black' : 'gray'}} >
+          <button style={styles.rightButton}>
+            Log in
+          </button>
+        </Link>
+      </div>
+    );
+  }
+
+  if (session) {
+    left = (
+      <div className="left">
+        <Link href="/" style={{fontWeight: 'bold', ...styles.nextLink, color: isActive('/') ? 'black' : 'gray'}}>
+            Feed
+        </Link>
+        <Link href="/drafts" style={{fontWeight: 'bold', ...styles.nextLink, color: isActive('/drafts') ? 'black' : 'gray'}}>
+          My drafts
+        </Link>
+        
+      </div>
+    );
+    right = (
+      <div style={styles.right}>
+        <p style={styles.p}>
+          {session.user.name} ({session.user.email})
+        </p>
+        <Link href="/create" style={styles.rightButton}>
+          <button style={{...styles.nextLink, ...styles.button}}>
+            New post
+          </button>
+        </Link>
+        <Link href="#" style={styles.rightButton}>
+          <button onClick={() => signOut()} style={{...styles.nextLink, ...styles.button}}>
+            Log out
+          </button>
+        </Link>
+      </div>
+    );
+  }
+
   return (
-    <nav>
+    <nav style={styles.nav}>
       {left}
       {right}
-      <style jsx>{`
-        nav {
-          display: flex;
-          padding: 2rem;
-          align-items: center;
-        }
-      `}</style>
     </nav>
   );
 };
 
 export default Header;
+
+const styles = {
+  nav: {
+    display: "flex",
+    padding: "2rem",
+    alignItems: "center"
+  },
+  nextLink: {
+    textDecoration: "none",
+    display: "inline-block",
+    margin: "0px 10px"
+  },
+  p: {
+    display: "inline-block",
+    fontSize: "13px",
+    paddingRight: "1rem"
+  },
+  right: {
+    marginLeft: "auto"
+  },
+  button: {
+    border: "none",
+    backgroundColor: "transparent"
+  },
+  rightButton: {
+    border: '1px solid black', 
+    padding: "0.5rem 1rem", 
+    borderRadius: "3px",
+    margin: "0 10px"
+  }
+}
