0b70dc82bd45514f3ff5d2630557df26c89bf68f
Tomy Visouthiphongs <tvisouthiphong@gmail.com>
2023-04-08 16:58:02 +0200

    Add publish & delete functionabilities

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,63 +1,92 @@
-import React from "react"
-import { GetServerSideProps } from "next"
-import ReactMarkdown from "react-markdown"
-import Layout from "../../components/Layout"
-import { PostProps } from "../../components/Post"
-import prisma from "../../lib/prisma"
+// pages/p/[id].tsx
+
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import ReactMarkdown from 'react-markdown';
+import Router from 'next/router';
+import Layout from '../../components/Layout';
+import { PostProps } from '../../components/Post';
+import { useSession } from 'next-auth/react';
+import prisma from '../../lib/prisma';
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-
   const post = await prisma.post.findUnique({
     where: {
-      id: String(params?.id)
+      id: String(params?.id),
     },
     include: {
       author: {
-        select: { name: true }
-      }
-    }
+        select: { name: true, email: true },
+      },
+    },
   });
   return {
     props: post,
-  }
+  };
+};
+
+async function publishPost(id: string): Promise<void> {
+  await fetch(`/api/publish/${id}`, {
+    method: 'PUT',
+  });
+  await Router.push('/');
+}
+
+async function deletePost(id: string): Promise<void> {
+  await fetch(`/api/post/${id}`, {
+    method: 'DELETE'
+  });
+  await Router.push('/');
 }
 
 const Post: React.FC<PostProps> = (props) => {
-  let title = props.title
+  const { data: session, status } = useSession();
+  if (status === 'loading') {
+    return <div>Authenticating ...</div>;
+  }
+  const userHasValidSession = Boolean(session);
+  const postBelongsToUser = session?.user?.email === props.author?.email;
+  let title = props.title;
   if (!props.published) {
-    title = `${title} (Draft)`
+    title = `${title} (Draft)`;
   }
 
   return (
     <Layout>
-      <div>
+      <div style={styles.page}>
         <h2>{title}</h2>
-        <p>By {props?.author?.name || "Unknown author"}</p>
+        <p>By {props?.author?.name || 'Unknown author'}</p>
         <ReactMarkdown children={props.content} />
+        {
+          !props.published && userHasValidSession && postBelongsToUser && (
+            <button onClick={() => publishPost(props.id)} style={styles.button}>Publish</button>
+          )
+        }
+        {
+          userHasValidSession && postBelongsToUser && (
+            <button onClick={() => deletePost(props.id)} style={styles.button}>Delete</button>
+          )
+        }
       </div>
-      <style jsx>{`
-        .page {
-          background: white;
-          padding: 2rem;
-        }
-
-        .actions {
-          margin-top: 2rem;
-        }
-
-        button {
-          background: #ececec;
-          border: 0;
-          border-radius: 0.125rem;
-          padding: 1rem 2rem;
-        }
-
-        button + button {
-          margin-left: 1rem;
-        }
-      `}</style>
     </Layout>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
+
+const styles = {
+  page: {
+    background: "white",
+    padding: "2rem"
+  },
+  actions: {
+    marginTop: "2rem"
+  },
+  button: {
+    background: "#ececec",
+    border: "0",
+    borderRadius: "0.125rem",
+    padding: "1rem 2rem",
+    marginLeft: "1rem"
+  }
+}
