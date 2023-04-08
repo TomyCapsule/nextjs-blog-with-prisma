ec175d991c354c1b59a781eccfdc23a4720007d0
Tomy Visouthiphongs <tvisouthiphong@gmail.com>
2023-04-08 16:32:37 +0200

    Clean imports

--- pages/create.tsx
+++ pages/create.tsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 import Layout from '../components/Layout';
 import Router from 'next/router';
 import Link from 'next/link';
-import axios from 'axios';
 
 const Draft: React.FC = () => {
   const [title, setTitle] = useState('');
